f63df5ceaa299ac32b482093b9d844681faa161a
fardinfaruk-bd <fardinfaruktofu990@gmail.com>
2026-05-03 04:31:09 +0600

    make the New arrivals Section responsive

--- src/components/Homepage/LatestBook.jsx
+++ src/components/Homepage/LatestBook.jsx
@@ -8,12 +8,12 @@ const LatestBook = async () => {
     const NewArrival = books.filter(book => book.is_new === true);
 
     return (
-        <div className='flex items-center gap-4 justify-between bg-[#F3F3F3] p-3 container mx-auto my-15  mt-15'>
+        <div className='flex flex-col md:flex-row items-center gap-4 justify-between bg-[#F3F3F3] p-3 container mx-auto my-15  mt-15'>
             <button className='btn bg-[#3DAAB0] text-white text-xl font-medium p-5'>New Arrivals:</button>
             <Marquee pauseOnHover={true} speed={100} className='text-[#403F3F] font-medium text-xl space-x-5'>
 
                 {NewArrival.map((n, i) => (
-                    <div key={i} className="mx-30  transition-all duration-500  transform hover:scale-110 flex flex-col items-center gap-3">
+                    <div key={i} className="mx-10 md:mx-30  transition-all duration-500  transform hover:scale-110 flex flex-col items-center gap-3">
                         <Image src={n.image_url} alt={n.title} width={120} height={120} className="object-contain" />
                         <p>{n.title}</p>
                     </div>
