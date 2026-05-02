a2ae24b12bf9b7ce69ce77858e95a06b8cc7bfe0
fardinfaruk-bd <fardinfaruktofu990@gmail.com>
2026-05-03 03:36:52 +0600

    Use swiperjs

--- src/components/Homepage/FeatureBook.jsx
+++ src/components/Homepage/FeatureBook.jsx
@@ -1,15 +1,40 @@
+'use client';
+
 import BookCard from '@/ui/BookCard';
 import React from 'react';
 
+// Swiper imports
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, Scrollbar } from 'swiper/modules';
+
+// Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
 const FeatureBook = ({ Books }) => {
     return (
-        <div className='container mx-auto space-y-5 mb-30'>
-            <h2 className='font-bold text-4xl text-center mb-10'>Feature Book</h2>
-            <div className='grid grid-cols-4 gap-5'>
-                {Books.slice(0, 4).map((book) => (
-                    <BookCard key={book.id} book={book} />
+        <div className='mx-auto space-y-5 mb-30'>
+            <h2 className='font-bold text-4xl text-center mb-10'>
+                Feature Book
+            </h2>
+            <Swiper
+                slidesPerView={3}
+                spaceBetween={30}
+                pagination={{
+                    clickable: true,
+                    
+                }}
+                modules={[Pagination]}
+                className="mySwiper"
+            >
+                {Books.slice(0, 10).map((book) => (
+                    <SwiperSlide key={book.id}>
+                        <BookCard book={book} />
+                    </SwiperSlide>
                 ))}
-            </div>
+            </Swiper>
+
         </div>
     );
 };
